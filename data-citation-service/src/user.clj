b6c776d764b715e181468cd9466a05c3cf431a3d
Dan <dan@eighttrigrams.net>
2023-01-10 22:36:26 +0000

    Provide REPLs for services

---
+++ data-citation-service/src/user.clj
@@ -0,0 +1,8 @@
+(ns user
+  (:require server))
+
+(prn "data-citation-service am start")
+
+(def n "data-citation-service")
+
+;; (server/-main)
---
+++ doi-service/src/user.clj
@@ -0,0 +1,8 @@
+(ns user
+  (:require server))
+
+(prn "doi-service am start")
+
+(def n "doi-service")
+
+;; (server/-main)
