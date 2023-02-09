6e62b3efdb9661b8a84b753d8f8134ff2482fb81
Dan <dan@eighttrigrams.net>
2023-02-09 21:14:40 +0000

    Implement img blob downloads

--- scraper/src/scraping.py
+++ scraper/src/scraping.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import urllib.parse
 import requests 
+import base64
 
 archive_path = 'archive/'
 
@@ -62,6 +63,38 @@ def download_css_files(soup, url, target):
 
     return soup
 
+# https://stackoverflow.com/a/47425305
+def get_file_content_chrome(driver, uri):
+  result = driver.execute_async_script("""
+    var uri = arguments[0];
+    var callback = arguments[1];
+    var toBase64 = function(buffer){for(var r,n=new Uint8Array(buffer),t=n.length,a=new Uint8Array(4*Math.ceil(t/3)),i=new Uint8Array(64),o=0,c=0;64>c;++c)i[c]="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".charCodeAt(c);for(c=0;t-t%3>c;c+=3,o+=4)r=n[c]<<16|n[c+1]<<8|n[c+2],a[o]=i[r>>18],a[o+1]=i[r>>12&63],a[o+2]=i[r>>6&63],a[o+3]=i[63&r];return t%3===1?(r=n[t-1],a[o]=i[r>>2],a[o+1]=i[r<<4&63],a[o+2]=61,a[o+3]=61):t%3===2&&(r=(n[t-2]<<8)+n[t-1],a[o]=i[r>>10],a[o+1]=i[r>>4&63],a[o+2]=i[r<<2&63],a[o+3]=61),new TextDecoder("ascii").decode(a)};
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function(){ callback(toBase64(xhr.response)) };
+    xhr.onerror = function(){ callback(xhr.status) };
+    xhr.open('GET', uri);
+    xhr.send();
+    """, uri)
+  if type(result) == int :
+    raise Exception("Request failed with status %s" % result)
+  return base64.b64decode(result)
+
+def download_img_blobs(driver, soup, url, target):
+    i = 0
+    for img in soup.find_all('img'):
+        i += 1
+        src = img.get('src')
+        if src.startswith('blob'):
+            print('yo', src)
+            path = target + '/' + str(i) + '.jpg'
+            print('yo', path)
+            with open(path, 'wb') as binary_file:
+                bytes = get_file_content_chrome(driver, src)
+                binary_file.write(bytes)
+            img['src'] = path
+    return soup
+
 def scrape(driver, url, target):
     driver.get(url)
     time.sleep(3)
@@ -72,6 +105,7 @@ def scrape(driver, url, target):
 
     os.mkdir(archive_path + target)
     soup = download_css_files(soup, url, archive_path + target)
+    soup = download_img_blobs(driver, soup, url, archive_path + target)
 
     with open(archive_path + target + '/index.html', 'w') as f:
         f.write(str(soup))
