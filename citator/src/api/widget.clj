657615130214233069646939800fbb74cec37ab7
Dan <dan@eighttrigrams.net>
2023-02-18 13:00:49 +0000

    Let widget return DOI of archived resource

--- citator/src/api/widget.clj
+++ citator/src/api/widget.clj
@@ -5,8 +5,18 @@
 ;; TODO use cljstache
 
 (defn get-form [req]
-  (let [referrer (get (:query-params req) "referrer")]
-    (str "<div style=\"background-color: darkred; color: white\">
+  (let [referrer (get (:query-params req) "referrer")
+        doi      (get (:query-params req) "doi")]
+    
+    (if doi
+
+      (str "<div style=\"background-color: darkred; color: white\">
+      <h1>Citator Widget</h1>
+      <b>Cite this entity with as: " doi " (DOI)</b> 
+      <p>Visit the DOI Registrar and search for this DOI do find 
+         the archived snapshot of this page</div>")
+      
+      (str "<div style=\"background-color: darkred; color: white\">
       <h1>Citator Widget</h1>
       <p>Take a snapshot and generate a DOI for this site</p>
       <form method=\"get\" action=\"/widget/request-archival\">
@@ -15,10 +25,10 @@
               name=\"" referrer "\">
        <input type=\"submit\" value=\"submit\"/>
       </form>
-    </div>")))
+    </div>"))))
 
 (defn submit-handler [req]
   (let [referrer (first (keys (:query-params req)))
         path     (str "/widget?referrer=" (java.net.URLEncoder/encode referrer))]
-    (scraping/archive! referrer)
-    (response/redirect path)))
+    (let [doi (scraping/archive! referrer)]
+      (response/redirect (str path "&doi=" doi)))))
