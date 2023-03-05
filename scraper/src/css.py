5f0b5d53a3ab00e77e29f372245c131988c5a72d
Dan <dan@eighttrigrams.net>
2023-03-05 14:16:51 +0000

    Adjust css.py for it to work with segmented paths, for propylaeum demo

--- scraper/src/css.py
+++ scraper/src/css.py
@@ -1,32 +1,45 @@
+import os
 import requests
+import urllib.parse
 from src import urls
-  
-def is_eligible_for_download(href):
-    return (href.endswith('css') and ((href.startswith('http') and href.count('/') == 3) 
-        or (href.startswith('/') and href.count('/') == 1) 
-        or href.count('/') == 0))
 
-def download_css_files(soup, url, resource_version_id):
-    url_without_path = urls.url_without_path(url)
+def get_new_href_and_download_path(base_url, href, i):
 
+    if not href.startswith("http"):
+        if not href.startswith("/"):
+            # this seems not to be correct (relative path), but is needed for the styles.css and main.css of the arachne entity pages
+            return "/" + href, urls.url_without_path(base_url) + "/" + href
+        return href, urls.url_without_path(base_url) + href
+
+    if base_url.startswith(urls.url_without_path(href)):
+        return href.replace(urls.url_without_path(href), ""), href
+    
+    # naive version to prevent us to have to url encode the url to be suitable as folder name
+    return "/" + str(i) + href.replace(urls.url_without_path(href), ""), href
+
+def download_css_files(soup, url, base_path):
+    i = 0
     for link in soup.find_all('link'):
         href = link.get('href')
-        if not is_eligible_for_download(href):
+        if (not href.endswith('css') 
+                # propylaeum test
+                and not href.endswith('?roxvr9')): 
             continue
-
-        url_path, download_path = urls.get_artifact_url(url_without_path, href)
-
-        link['href'] = resource_version_id + url_path
-
-        if not urls.url_with_simple_path(download_path):
-            continue;
         
-        # TODO isn't this equal to the url_path?
-        artifact = download_path.split('/')[3]
+        # propylaeum test
+        href = href.replace("?roxvr9", "")
+
+        new_href, download_path = get_new_href_and_download_path(url, href, i)
+
+        relative_path = base_path + new_href
 
         r = requests.get(download_path)
         if r.status_code == 200:
-            with open(resource_version_id + '/' + artifact, 'w') as f:
+            folder, file = os.path.split(relative_path)
+            os.makedirs(folder, exist_ok=True)
+            with open(relative_path, 'w') as f:
                 f.write(r.text)
 
+        link['href'] = "/" + relative_path
+        i += 1
     return soup
