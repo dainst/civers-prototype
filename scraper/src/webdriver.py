3b9cd4a249fafe6d766bdea44b7c6cf5515b9bbb
Marcel Riedel <Riedel-Marcel@gmx.de>
2023-03-06 10:12:28 +0100

    add disable-dev-shm-ussage option
    https://stackoverflow.com/questions/53902507/unknown-error-session-deleted-because-of-page-crash-from-unknown-error-cannot

--- scraper/src/webdriver.py
+++ scraper/src/webdriver.py
@@ -6,4 +6,5 @@ def get_chrome_driver():
     chrome_options.add_argument('--window-size=1220,960')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     return webdriver.Chrome(chrome_options=chrome_options)
